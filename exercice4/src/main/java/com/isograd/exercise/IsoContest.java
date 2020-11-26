8298cd258d68a9a885efb6f511d897989c3d25d4
Samuel Khalgui <samuel.khalgui@up.coop>
2020-11-26 23:25:27 +0100

    exercice 4

--- exercice4/src/main/java/com/isograd/exercise/IsoContest.java
+++ exercice4/src/main/java/com/isograd/exercise/IsoContest.java
@@ -19,21 +19,27 @@ public class IsoContest {
         int M = Integer.parseInt(split[1]);
         line = sc.nextLine();
         split = line.split(" ");
+        // algo trouvé sur :
+        // https://www.geeksforgeeks.org/xor-of-a-subarray-range-of-elements-set-2/?ref=rp
         int[] octets = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+        for (int i = 1; i < octets.length; i++) {
+            octets[i] = octets[i] ^ octets[i - 1];
+        }
         int[] result = new int[256];
         while (sc.hasNextLine()) {
             line = sc.nextLine();
             split = line.split(" ");
             int L = Integer.parseInt(split[0]);
             int R = Integer.parseInt(split[1]);
-            int x = octets[L];
-            for (int i = L + 1; i <= R; i++) {
-                x = x ^ octets[i];
+            int x;
+            if (L == 0) {
+                x = octets[R];
+            } else {
+                x = octets[L - 1] ^ octets[R];
             }
-            result[x] ++;
-            /* Lisez les données et effectuez votre traitement */
+            result[x]++;
         }
-        System.out.println(Arrays.stream(result).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
         /* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+        System.out.println(Arrays.stream(result).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
     }
 }
